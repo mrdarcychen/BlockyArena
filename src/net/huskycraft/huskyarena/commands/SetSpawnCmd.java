453a3c1cb1b23f75f59ec293d6119b2e8a1dd239
Darcy Chen <darcy.chen@outlook.com>
2017-09-07 23:30:26 -0700

    Initialize DoneCmd class and refine SetSpawnCmd class

--- src/net/huskycraft/huskyarena/commands/SetSpawnCmd.java
+++ src/net/huskycraft/huskyarena/commands/SetSpawnCmd.java
@@ -9,6 +9,7 @@ import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.command.spec.CommandExecutor;
 import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.text.Text;
 import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
 
@@ -26,14 +27,23 @@ public class SetSpawnCmd implements CommandExecutor{
     public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
 
         Player player = (Player)src;
-        UUID uuid = player.getUniqueId();
-        Arena arena = plugin.getArenaManager().arenaCreators.get(uuid);
+
+        if (!plugin.getArenaManager().arenaCreators.containsKey(player.getUniqueId())) {
+            player.sendMessage(Text.of("You're not currently creating an arena."));
+            return CommandResult.empty();
+        }
+
+        Arena arena = plugin.getArenaManager().arenaCreators.get(player.getUniqueId());
         Location<World> spawn = player.getLocation();
+        String type = args.getOne("type").get().toString();
+
         try {
-            arena.setSpawn(args.getOne("type").get().toString(), spawn);
+            arena.setSpawn(type, spawn);
         } catch (ObjectMappingException e) {
             e.printStackTrace();
         }
+        player.sendMessage(Text.of("Successfully set " + type + " spawn at " +
+                spawn.getPosition().round().toInt()));
 
         return CommandResult.success();
     }
