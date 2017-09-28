77d9d6c277bae085a92073605c0827def05d29a3
Darcy Chen <mrdarcychen@gmail.com>
2017-09-28 10:21:23 -0700

    Modify CreateCmd

--- src/main/java/net/huskycraft/blockyarena/commands/CreateCmd.java
+++ src/main/java/net/huskycraft/blockyarena/commands/CreateCmd.java
@@ -2,12 +2,15 @@ package net.huskycraft.blockyarena.commands;
 
 import net.huskycraft.blockyarena.Arena;
 import net.huskycraft.blockyarena.BlockyArena;
+import net.huskycraft.blockyarena.PlayerClass;
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.command.spec.CommandExecutor;
 import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.item.inventory.Inventory;
+import org.spongepowered.api.item.inventory.type.GridInventory;
 import org.spongepowered.api.text.Text;
 
 import java.util.UUID;
@@ -24,14 +27,24 @@ public class CreateCmd implements CommandExecutor {
     public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
 
         Player player = (Player)src;
-        UUID uuid = player.getUniqueId();
+        String object = args.<String>getOne("object").get();
         String name = args.<String>getOne("name").get();
-        Arena arena = new Arena(plugin, name);
-        plugin.getArenaManager().arenaCreators.put(uuid, arena);
-        player.sendMessage(Text.of("Start creating arena " + name + ".\n" +
-                "Use `/arena setspawn <type>` to set spawn points. \n" +
-                "Use `/arena done` to save."));
 
+        switch (object) {
+            case "arena":
+                UUID uuid = player.getUniqueId();
+                Arena arena = new Arena(plugin, name);
+                plugin.getArenaManager().arenaCreators.put(uuid, arena);
+                player.sendMessage(Text.of("Start creating arena " + name + ".\n" +
+                        "Use `/arena setspawn <type>` to set spawn points. \n" +
+                        "Use `/arena done` to save."));
+                break;
+            case "class":
+                PlayerClass playerClass = new PlayerClass(plugin, name, player.getInventory().query(GridInventory.class));
+                break;
+            default:
+                player.sendMessage(Text.of("Invalid type of object."));
+        }
         return CommandResult.success();
     }
 }
