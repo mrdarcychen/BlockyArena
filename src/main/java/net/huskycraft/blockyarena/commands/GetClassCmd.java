711a04a9efc557eb2a5b3c79775e26d55386131d
Darcy Chen <darcy.chen@outlook.com>
2017-09-30 10:39:06 -0700

    Block player from choosing class under few circumstances

--- src/main/java/net/huskycraft/blockyarena/commands/GetClassCmd.java
+++ src/main/java/net/huskycraft/blockyarena/commands/GetClassCmd.java
@@ -20,8 +20,20 @@ public class GetClassCmd implements CommandExecutor{
     @Override
     public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
         Player player = (Player) src;
+        // if player is not in any session, return empty
+        if (!plugin.getSessionManager().playerSession.containsKey(player)) {
+            player.sendMessage(Text.of("You are not allowed to pick a class outside of sessions"));
+            return CommandResult.empty();
+        }
+        // if game has already started, return empty
+        if (!plugin.getSessionManager().playerSession.get(player).canJoin) {
+            player.sendMessage(Text.of("You are not allowed to pick a class now."));
+            return CommandResult.empty();
+        }
+
         String className = args.<String>getOne(Text.of("name")).get();
         PlayerClass playerClass = plugin.getPlayerClassManager().getPlayerClass(className);
+        player.getInventory().clear();
         playerClass.offerItemStacksTo(player);
         player.sendMessage(Text.of("You are now " + className));
         return CommandResult.success();
