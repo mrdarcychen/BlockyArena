104a53e87e4440797c613a57ef920b89fac08756
Darcy Chen <mrdarcychen@gmail.com>
2017-11-18 19:57:22 -0800

    Refine structure for SessionManager class

--- src/main/java/net/huskycraft/blockyarena/managers/SessionManager.java
+++ src/main/java/net/huskycraft/blockyarena/managers/SessionManager.java
@@ -3,33 +3,29 @@ package net.huskycraft.blockyarena.managers;
 import net.huskycraft.blockyarena.Arena;
 import net.huskycraft.blockyarena.BlockyArena;
 import net.huskycraft.blockyarena.Session;
-import org.spongepowered.api.entity.living.player.Player;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.List;
 
 public class SessionManager {
 
     BlockyArena plugin;
 
-    public ArrayList<Session> sessions;
-
-    public HashMap<Player, Session> playerSession;
+    private List<Session> sessions;
 
     public SessionManager(BlockyArena plugin) {
         this.plugin = plugin;
         sessions = new ArrayList<>();
-        playerSession = new HashMap<>();
-
     }
 
-    /*
-    returns a session that matches an arena and is still in lobby wait period
+    /**
+     * Returns an available session.
+     * @return null if no session is available
      */
     public Session getAvailableSession() {
         if (sessions.size() != 0) {
             for (Session session : sessions) {
-                if (session.canJoin) {
+                if (session.canJoin()) {
                     return session;
                 }
             }
