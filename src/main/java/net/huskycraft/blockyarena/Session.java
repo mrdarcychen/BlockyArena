67e727532444f05c7d2198ae842f669a427719c1
Darcy Chen <mrdarcychen@gmail.com>
2017-11-18 18:31:41 -0800

    Add countdown and checkPreCond methods for Session class

--- src/main/java/net/huskycraft/blockyarena/Session.java
+++ src/main/java/net/huskycraft/blockyarena/Session.java
@@ -1,15 +1,29 @@
 package net.huskycraft.blockyarena;
 
+import org.spongepowered.api.effect.sound.SoundTypes;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.scheduler.Task;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.title.Title;
+
 import java.util.HashSet;
 import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.concurrent.TimeUnit;
 
 public abstract class Session {
 
-    private Set<Gamer> gamers;
+    public static BlockyArena plugin;
 
-    public Session() {
+    private Set<Gamer> gamers;      // the set of gamers in the session
+
+    private Task timer;     // the countdown timer before the start of the game
+
+    private boolean canJoin;
+
+    private int minPlayer, maxPlayer;
+
+    public Session(BlockyArena plugin) {
+        this.plugin = plugin;
         gamers = new HashSet();
     }
 
@@ -20,6 +34,7 @@ public abstract class Session {
         gamers.add(gamer);
         // TODO: backups the gamer's inventory and clear it
         // TODO: sends the gamer to the waiting area
+        checkPreCond();
     }
 
     /**
@@ -27,7 +42,46 @@ public abstract class Session {
      */
     public void remove(Gamer gamer) {
         gamers.remove(gamer);
-        // TODO: sends the gamer to his join location / main lobby
-        // TODO: gives back the player's inventory
+        // TODO: send the gamer to his join location / main lobby
+        // TODO: give back the player's inventory
+        if (canJoin) {
+            checkPreCond();
+        }
+    }
+
+    /**
+     * Checks the precondition of the game.
+     * Starts the countdown if the precondition is met, cancels the countdown otherwise.
+     */
+    public void checkPreCond() {
+        if (gamers.size() < minPlayer && timer != null) {
+            canJoin = true;
+            timer.cancel();
+        } else if (gamers.size() >= minPlayer) {
+            canJoin = false;
+            countdown(10);  // TODO: to be customized
+        }
+    }
+
+    /**
+     * Displays the countdown title in seconds to all gamers. Starts the game when countdown is 0.
+     * @param second the seconds remaining for the countdown
+     * @return true if the countdown is over, false otherwise
+     */
+    private void countdown(int second) {
+        if (second == 0) {
+            timer = null;
+            // TODO: start the game
+        } else {
+            Title title = Title.builder()
+                    .title(Text.of(second)).fadeIn(2).fadeOut(2).stay(16).build();
+            for (Gamer gamer : gamers) {
+                Player player = gamer.getPlayer();
+                player.sendTitle(title);
+                player.playSound(SoundTypes.BLOCK_DISPENSER_DISPENSE, player.getHeadRotation(), 100);
+            }
+        }
+        timer = Task.builder()
+                .execute(() -> countdown(second - 1)).delay(1, TimeUnit.SECONDS).submit(plugin);
     }
 }
