297ea791326012a082b730f1093c2f4827973a80
Darcy Chen <darcy.chen@outlook.com>
2018-01-12 14:57:31 -0800

    Refine fields, constructor, and add and spawn methods of Team class

--- src/main/java/net/huskycraft/blockyarena/Team.java
+++ src/main/java/net/huskycraft/blockyarena/Team.java
@@ -1,17 +1,44 @@
 package net.huskycraft.blockyarena;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.flowpowered.math.vector.Vector3d;
+import org.spongepowered.api.world.Location;
+import org.spongepowered.api.world.World;
 
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * The Team class represents a group of gamers in a Game that share the same spawn point.
+ */
 public class Team {
 
-    private List<Gamer> gamers;
+    private Location<World> spawnLocation; // the spawn location of the team
+    private Vector3d spawnRotation; // the spawn rotation of the team
+    private Set<Gamer> gamers; // the set of gamers in the team
 
-    public Team() {
-        gamers = new ArrayList<>();
+    /**
+     * Constructs a Team with the location of the spawn point and the rotation of the spawn point
+     * @param spawnLocation the location that a player spawns
+     * @param spawnRotation the heading of a player who spawns at the spawn location
+     */
+    public Team(Location<World> spawnLocation, Vector3d spawnRotation) {
+        this.spawnLocation = spawnLocation;
+        this.spawnRotation = spawnRotation;
+        gamers = new TreeSet<>();
     }
 
+    /**
+     * Adds the given gamer to this Team and send the gamer to the spawn location.
+     */
     public void add(Gamer gamer) {
         gamers.add(gamer);
+        spawn(gamer);
+    }
+
+    /**
+     * Spawns the given gamer at the spawn location of the team.
+     */
+    public void spawn(Gamer gamer) {
+        gamer.getPlayer().setLocationAndRotation(spawnLocation, spawnRotation);
     }
 }
