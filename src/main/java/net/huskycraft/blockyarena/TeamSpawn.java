13840a1349468b9fa63491b310a864e5ad5a4dcf
Darcy Chen <darcy.chen@outlook.com>
2018-01-12 15:01:43 -0800

    Rename class Team to TeamSpawn

--- src/main/java/net/huskycraft/blockyarena/TeamSpawn.java
+++ src/main/java/net/huskycraft/blockyarena/TeamSpawn.java
@@ -10,29 +10,19 @@ import java.util.TreeSet;
 /**
  * The Team class represents a group of gamers in a Game that share the same spawn point.
  */
-public class Team {
+public class TeamSpawn {
 
     private Location<World> spawnLocation; // the spawn location of the team
     private Vector3d spawnRotation; // the spawn rotation of the team
-    private Set<Gamer> gamers; // the set of gamers in the team
 
     /**
      * Constructs a Team with the location of the spawn point and the rotation of the spawn point
      * @param spawnLocation the location that a player spawns
      * @param spawnRotation the heading of a player who spawns at the spawn location
      */
-    public Team(Location<World> spawnLocation, Vector3d spawnRotation) {
+    public TeamSpawn(Location<World> spawnLocation, Vector3d spawnRotation) {
         this.spawnLocation = spawnLocation;
         this.spawnRotation = spawnRotation;
-        gamers = new TreeSet<>();
-    }
-
-    /**
-     * Adds the given gamer to this Team and send the gamer to the spawn location.
-     */
-    public void add(Gamer gamer) {
-        gamers.add(gamer);
-        spawn(gamer);
     }
 
     /**
