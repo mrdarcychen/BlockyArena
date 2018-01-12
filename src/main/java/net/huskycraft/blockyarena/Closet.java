72f279469c268449efd4c56eb46511e35f4ea9cb
Darcy Chen <mrdarcychen@gmail.com>
2018-01-11 22:47:51 -0800

    Support storing mod items in Closet

--- src/main/java/net/huskycraft/blockyarena/Closet.java
+++ src/main/java/net/huskycraft/blockyarena/Closet.java
@@ -5,6 +5,7 @@ import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.item.inventory.ItemStack;
 import org.spongepowered.api.item.inventory.Slot;
 import org.spongepowered.api.item.inventory.entity.PlayerInventory;
+import org.spongepowered.api.item.inventory.equipment.EquipmentTypes;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,13 +18,11 @@ public class Closet {
 
     public static BlockyArena plugin;
 
-    private Optional<ItemStack> helmet, chestplate, leggings, boots; // ItemStacks in the equipment inventory
-    private List<ItemStack> hotbar; // ItemStacks in the hotbar inventory
-    private List<ItemStack> main; // ItemStacks in the main inventory
+    private List<ItemStack> hotbar;
+    private List<ItemStack> main;
+    private Optional<ItemStack> offHand;
+    private Optional<ItemStack> helmet, chestplate, leggings, boots;
 
-    /**
-     * Constructs a Closet based on the inventory of the given player.
-     */
     public Closet(Player player) {
         hotbar = new ArrayList<>();
         main = new ArrayList<>();
@@ -40,52 +39,26 @@ public class Closet {
                 main.add(slot.peek().get());
             }
         }
-        helmet = player.getHelmet();
-        chestplate = player.getChestplate();
-        leggings = player.getLeggings();
-        boots = player.getBoots();
+        helmet = inv.getEquipment().getSlot(EquipmentTypes.HEADWEAR).get().peek();
+        chestplate = inv.getEquipment().getSlot(EquipmentTypes.CHESTPLATE).get().peek();
+        leggings = inv.getEquipment().getSlot(EquipmentTypes.LEGGINGS).get().peek();
+        boots = inv.getEquipment().getSlot(EquipmentTypes.BOOTS).get().peek();
+        offHand = inv.getOffhand().peek();
     }
 
-    /**
-     * Gives all ItemStacks stored in the Closet to the given player.
-     * This function will not replace any item in the player's inventory.
-     */
-    public void equipAll(Player player) {
-        equipArmors(player);
-        equipHotbar(player);
-        equipMain(player);
-    }
-
-    /**
-     * Gives ItemStacks stored in the hotbar inventory to the given player.
-     * This function will not replace any item in the player's inventory.
-     */
-    public void equipHotbar(Player player) {
+    public void equip(Player player) {
         PlayerInventory inv = (PlayerInventory) player.getInventory();
+
         for (ItemStack itemStack : hotbar) {
             inv.getHotbar().offer(itemStack.copy());
         }
-    }
-
-    /**
-     * Gives ItemStacks stored in the main grid inventory to the given player.
-     * This function will not replace any item in the player's inventory.
-     */
-    public void equipMain(Player player) {
-        PlayerInventory inv = (PlayerInventory) player.getInventory();
         for (ItemStack itemStack : main) {
             inv.getMainGrid().offer(itemStack.copy());
         }
-    }
-
-    /**
-     * Give ItemStacks stored in the equipment inventory to the given player.
-     * This function will not replace any item in the player's inventory.
-     */
-    public void equipArmors(Player player) {
         if (helmet.isPresent()) player.setHelmet(helmet.get().copy());
         if (chestplate.isPresent()) player.setChestplate(chestplate.get().copy());
         if (leggings.isPresent()) player.setLeggings(leggings.get().copy());
         if (boots.isPresent()) player.setBoots(boots.get().copy());
+        if (offHand.isPresent()) inv.getOffhand().offer(offHand.get().copy());
     }
 }
