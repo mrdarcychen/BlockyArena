1aa1178e14def18c4a4667c564e103e4e6ea0c68
Darcy Chen <mrdarcychen@gmail.com>
2018-01-18 18:22:59 -0800

    Add lobbyspawn field and relative functionalities to Arena class

--- src/main/java/net/huskycraft/blockyarena/Arena.java
+++ src/main/java/net/huskycraft/blockyarena/Arena.java
@@ -13,79 +13,101 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+/**
+ * An Arena represents an specific region for dueling.
+ */
 public class Arena {
 
     public static BlockyArena plugin;
 
-    private Path arenaConfig; // the config file of this Arena
+    private Path config; // the config file of the Arena
 
-    private String ID;
-    private TeamSpawn teamSpawnA; // the TeamSpawn data for team A
-    private TeamSpawn teamSpawnB; // the TeamSpawn data for team B
+    private String ID; // the unique identification code of the Arena
+    private Spawn teamSpawnA; // the spawn point for team A
+    private Spawn teamSpawnB; // the spawn point for team B
+    private Spawn lobbySpawn; // the optional common spawn point for all players
 
     /**
-     * Constructs an Arena with a new ID and two TeamSpawns.
+     * Constructs an arena with an ID and two team spawns.
+     * @param ID a unique identification code of the Arena
+     *           assuming that no existing arena has the same ID
+     * @param teamSpawnA a spawn point for team A
+     * @param teamSpawnB a spawn point for team B
      */
-    public Arena(String ID, TeamSpawn teamSpawnA, TeamSpawn teamSpawnB) {
+    public Arena(String ID, Spawn teamSpawnA, Spawn teamSpawnB) {
+        this(ID, teamSpawnA, teamSpawnB, null);
+    }
+
+    /**
+     * Constructs a regular arena with an ID and three spawns.
+     * @param ID a unique identification code of the Arena
+     *           assuming that no existing arena has the same ID
+     * @param teamSpawnA a spawn point for team A
+     * @param teamSpawnB a spawn point for team B
+     * @param lobbySpawn an optional common spawn point for all players
+     */
+    public Arena(String ID, Spawn teamSpawnA, Spawn teamSpawnB, Spawn lobbySpawn) {
         this.ID = ID;
         this.teamSpawnA = teamSpawnA;
         this.teamSpawnB = teamSpawnB;
+        this.lobbySpawn = lobbySpawn;
 
-        arenaConfig = Paths.get(plugin.getArenaDir().toString() + File.separator + ID + ".conf");
+        config = Paths.get(plugin.getArenaDir().toString() + File.separator + ID + ".conf");
         try {
-            if (!arenaConfig.toFile().exists()) {
-                Files.createFile(arenaConfig);
+            if (!config.toFile().exists()) {
+                Files.createFile(config);
             }
         } catch (IOException e) {
             plugin.getLogger().warn("Error creating arena config file for " + ID);
         }
-        writeConfig(arenaConfig);
+        writeConfig(config);
     }
 
     /**
-     * Reconstructs an Arena from an existing arena config file.
+     * Reconstructs an arena from an existing arena config file.
+     * @param config a config file storing an arena's data in standard format
      */
-    public Arena(Path arenaConfig) {
-        this.arenaConfig = arenaConfig;
-        readConfig(arenaConfig);
+    public Arena(Path config) {
+        this.config = config;
+        readConfig(config);
     }
-
 
     /**
      * Updates the given arena config file based on the object's data fields.
-     * @param arenaConfig
+     * @param config
      */
-    public void writeConfig(Path arenaConfig) {
+    public void writeConfig(Path config) {
         ConfigurationLoader<CommentedConfigurationNode> loader = HoconConfigurationLoader
-                .builder().setPath(arenaConfig).build();
+                .builder().setPath(config).build();
 
         try {
             ConfigurationNode rootNode = loader.load();
             rootNode.getNode("id").setValue(ID);
-            rootNode.getNode("teamSpawnA").setValue(TypeToken.of(TeamSpawn.class), teamSpawnA);
-            rootNode.getNode("teamSpawnB").setValue(TypeToken.of(TeamSpawn.class), teamSpawnB);
+            rootNode.getNode("teamSpawnA").setValue(TypeToken.of(Spawn.class), teamSpawnA);
+            rootNode.getNode("teamSpawnB").setValue(TypeToken.of(Spawn.class), teamSpawnB);
+            rootNode.getNode("lobbySpawn").setValue(TypeToken.of(Spawn.class), lobbySpawn);
             loader.save(rootNode);
         } catch (IOException e) {
             plugin.getLogger().warn("Error writing arena config.");
         } catch (ObjectMappingException e) {
 
         }
-
     }
 
     /**
      * Updates the fields of this object by reading the given config file.
-     * @param arenaConfig
+     * @param config
      */
-    public void readConfig(Path arenaConfig) {
+    public void readConfig(Path config) {
         ConfigurationLoader<CommentedConfigurationNode> loader = HoconConfigurationLoader
-                .builder().setPath(arenaConfig).build();
+                .builder().setPath(config).build();
 
         try {
             ConfigurationNode rootNode = loader.load();
             ID = rootNode.getNode("id").getString();
-            teamSpawnA = rootNode.getNode("teamSpawnA").getValue(TypeToken.of(TeamSpawn.class));
-            teamSpawnB = rootNode.getNode("teamSpawnB").getValue(TypeToken.of(TeamSpawn.class));
+            teamSpawnA = rootNode.getNode("teamSpawnA").getValue(TypeToken.of(Spawn.class));
+            teamSpawnB = rootNode.getNode("teamSpawnB").getValue(TypeToken.of(Spawn.class));
+            lobbySpawn = rootNode.getNode("lobbySpawn").getValue(TypeToken.of(Spawn.class));
             loader.save(rootNode);
         } catch (IOException e) {
             plugin.getLogger().warn("Error reading arena config.");
