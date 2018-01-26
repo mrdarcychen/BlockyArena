317af3e35c9dfe2e0fab655aea3e203771be2577
Darcy Chen <darcy.chen@outlook.com>
2018-01-25 23:12:05 -0800

    Update getActiveSession method in SessionManager

--- src/main/java/net/huskycraft/blockyarena/managers/SessionManager.java
+++ src/main/java/net/huskycraft/blockyarena/managers/SessionManager.java
@@ -1,39 +1,37 @@
 package net.huskycraft.blockyarena.managers;
 
-import net.huskycraft.blockyarena.Arena;
-import net.huskycraft.blockyarena.BlockyArena;
-import net.huskycraft.blockyarena.Session;
+import net.huskycraft.blockyarena.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class SessionManager {
 
-    BlockyArena plugin;
+    public static BlockyArena plugin;
 
     private List<Session> sessions;
 
-    public SessionManager(BlockyArena plugin) {
-        this.plugin = plugin;
+    public SessionManager() {
         sessions = new ArrayList<>();
     }
 
     /**
-     * Gets an available session.
+     * Gets an available active session from the list.
      * @return null if no session is available
      */
-    public Session getAvailableSession() {
-        if (sessions.size() != 0) {
-            for (Session session : sessions) {
-                if (session.canJoin()) {
-                    return session;
-                }
+    public Session getActiveSession(TeamType teamType) {
+        for (Session session : sessions) {
+            boolean isActive = session.getState() == SessionState.ACTIVE;
+            boolean isGivenType = session.getTeamType() == teamType;
+            if (isActive && isGivenType) {
+                return session;
             }
         }
 
+        // if no session is active
         Arena arena = plugin.getArenaManager().getAvailableArena();
         if (arena == null) return null;
-        Session session = new Session(plugin, arena);
+        Session session = new Session(arena, teamType);
         sessions.add(session);
         return session;
     }
