44173fd14c65149621fff9c1bdd0b3caa37aef85
Darcy Chen <mrdarcychen@gmail.com>
2018-02-06 18:49:04 -0800

    Modify TeamType class to TeamMode class

---
+++ src/main/java/net/huskycraft/blockyarena/TeamMode.java
@@ -0,0 +1,8 @@
+package net.huskycraft.blockyarena;
+
+/**
+ * A TeamMode represents the mode of a Team based on the number of gamers on a Team.
+ */
+public enum TeamMode {
+    SOLO, DOUBLES, TEAMS;
+}
