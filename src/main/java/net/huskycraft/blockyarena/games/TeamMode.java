bf90bc4dae59d8f6f8fc71f2e5cd63ab9b3f39fa
Darcy Chen <mrdarcychen@gmail.com>
2018-02-18 00:24:39 -0800

    Refine fields and structures of Kit class (originally Closet)

--- src/main/java/net/huskycraft/blockyarena/games/TeamMode.java
+++ src/main/java/net/huskycraft/blockyarena/games/TeamMode.java
@@ -4,5 +4,5 @@ package net.huskycraft.blockyarena.games;
  * A TeamMode represents the mode of a Team based on the number of gamers on a Team.
  */
 public enum TeamMode {
-    SOLO, DOUBLES, TEAMS;
+    SOLO, DOUBLES, TEAMS
 }
