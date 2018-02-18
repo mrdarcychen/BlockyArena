0412b0982b286e892f1727233fc5a36ddc2af835
Darcy Chen <darcy.chen@outlook.com>
2018-02-18 15:03:22 -0800

    Enable writing/reading Kit to/from config

--- src/main/java/net/huskycraft/blockyarena/managers/KitManager.java
+++ src/main/java/net/huskycraft/blockyarena/managers/KitManager.java
@@ -1,8 +1,20 @@
 package net.huskycraft.blockyarena.managers;
 
+import com.google.common.reflect.TypeToken;
 import net.huskycraft.blockyarena.BlockyArena;
 import net.huskycraft.blockyarena.utils.Kit;
+import ninja.leaping.configurate.ConfigurationNode;
+import ninja.leaping.configurate.commented.CommentedConfigurationNode;
+import ninja.leaping.configurate.hocon.HoconConfigurationLoader;
+import ninja.leaping.configurate.loader.ConfigurationLoader;
+import ninja.leaping.configurate.objectmapping.ObjectMappingException;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,6 +29,25 @@ public class KitManager {
     public KitManager(BlockyArena plugin) {
         this.plugin = plugin;
         kits = new HashMap<>();
+        loadKits();
+    }
+
+    private void loadKits() {
+        try {
+            DirectoryStream<Path> stream = Files.newDirectoryStream(plugin.getKitDir(), "*.conf");
+            for (Path path : stream) {
+                ConfigurationLoader<CommentedConfigurationNode> loader = HoconConfigurationLoader
+                        .builder().setPath(path).build();
+                ConfigurationNode rootNode = loader.load();
+                Kit kit = rootNode.getValue(TypeToken.of(Kit.class));
+                kits.put(kit.getId(), kit);
+                loader.save(rootNode);
+            }
+        } catch (ObjectMappingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -28,6 +59,18 @@ public class KitManager {
     public void add(Kit kit, String id) {
         kits.put(id, kit);
         plugin.getLogger().warn(id + " has been added to kit manager.");
+        Path path = Paths.get(plugin.getKitDir().toString() + File.separator + id + ".conf");
+        ConfigurationLoader<CommentedConfigurationNode> loader = HoconConfigurationLoader
+                .builder().setPath(path).build();
+        try {
+            ConfigurationNode rootNode = loader.load();
+            rootNode.setValue(TypeToken.of(Kit.class), kit);
+            loader.save(rootNode);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ObjectMappingException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -37,7 +80,6 @@ public class KitManager {
      * @return the Kit with the given id
      */
     public Kit get(String id) {
-        plugin.getLogger().warn("Contains " + id + "? " + kits.containsKey(id));
         return kits.get(id);
     }
 }
---
+++ src/main/java/net/huskycraft/blockyarena/utils/KitSerializer.java
@@ -0,0 +1,55 @@
+package net.huskycraft.blockyarena.utils;
+
+import com.google.common.reflect.TypeToken;
+import net.huskycraft.blockyarena.BlockyArena;
+import ninja.leaping.configurate.ConfigurationNode;
+import ninja.leaping.configurate.objectmapping.ObjectMappingException;
+import ninja.leaping.configurate.objectmapping.serialize.TypeSerializer;
+import org.spongepowered.api.item.inventory.ItemStack;
+import org.spongepowered.api.item.inventory.property.SlotIndex;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class KitSerializer implements TypeSerializer<Kit> {
+
+    public static BlockyArena plugin;
+
+    public KitSerializer(BlockyArena plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public Kit deserialize(TypeToken<?> type, ConfigurationNode value) throws ObjectMappingException {
+        String id = value.getNode("id").getString();
+        Optional<ItemStack> headwear = Optional.of(value.getNode("headwear").getValue(TypeToken.of(ItemStack.class)));
+        Optional<ItemStack> chestplate = Optional.of(value.getNode("chestplate").getValue(TypeToken.of(ItemStack.class)));
+        Optional<ItemStack> leggings = Optional.of(value.getNode("leggings").getValue(TypeToken.of(ItemStack.class)));
+        Optional<ItemStack> boots = Optional.of(value.getNode("boots").getValue(TypeToken.of(ItemStack.class)));
+        Optional<ItemStack> offHand = Optional.of(value.getNode("offHand").getValue(TypeToken.of(ItemStack.class)));
+        Map<SlotIndex, ItemStack> main = new HashMap<>();
+        for (ConfigurationNode indexNode : value.getNode("main").getChildrenMap().values()) {
+            SlotIndex index = SlotIndex.of(indexNode.getKey());
+            ItemStack itemStack = indexNode.getValue(TypeToken.of(ItemStack.class));
+            main.put(index, itemStack);
+        }
+        return new Kit(id, main, headwear, chestplate, leggings, boots, offHand);
+    }
+
+    @Override
+    public void serialize(TypeToken<?> type, Kit obj, ConfigurationNode value) throws ObjectMappingException {
+        value.getNode("id").setValue(obj.getId());
+        value.getNode("headwear").setValue(TypeToken.of(ItemStack.class), obj.getHeadwear().get());
+        value.getNode("chestplate").setValue(TypeToken.of(ItemStack.class), obj.getChestplate().get());
+        value.getNode("leggings").setValue(TypeToken.of(ItemStack.class), obj.getLeggings().get());
+        value.getNode("boots").setValue(TypeToken.of(ItemStack.class), obj.getBoots().get());
+        value.getNode("offHand").setValue(TypeToken.of(ItemStack.class), obj.getOffHand().get());
+        for (SlotIndex index : obj.getMain().keySet()) {
+            plugin.getLogger().warn("index: " + index.getValue().toString());
+            plugin.getLogger().warn("item:" + obj.getMain().get(index).getType().toString());
+            ConfigurationNode itemNode = value.getNode("main").getNode(index.getValue().toString());
+            itemNode.setValue(TypeToken.of(ItemStack.class), obj.getMain().get(index));
+        }
+    }
+}
