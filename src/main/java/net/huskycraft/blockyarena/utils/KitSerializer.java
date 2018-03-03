94754dfcc3e7fd0babd6626991767d3f75dc5b72
Darcy Chen <mrdarcychen@gmail.com>
2018-03-02 23:49:29 -0800

    Fix #13

--- src/main/java/net/huskycraft/blockyarena/utils/KitSerializer.java
+++ src/main/java/net/huskycraft/blockyarena/utils/KitSerializer.java
@@ -23,11 +23,16 @@ public class KitSerializer implements TypeSerializer<Kit> {
     @Override
     public Kit deserialize(TypeToken<?> type, ConfigurationNode value) throws ObjectMappingException {
         String id = value.getNode("id").getString();
-        Optional<ItemStack> headwear = Optional.of(value.getNode("headwear").getValue(TypeToken.of(ItemStack.class)));
-        Optional<ItemStack> chestplate = Optional.of(value.getNode("chestplate").getValue(TypeToken.of(ItemStack.class)));
-        Optional<ItemStack> leggings = Optional.of(value.getNode("leggings").getValue(TypeToken.of(ItemStack.class)));
-        Optional<ItemStack> boots = Optional.of(value.getNode("boots").getValue(TypeToken.of(ItemStack.class)));
-        Optional<ItemStack> offHand = Optional.of(value.getNode("offHand").getValue(TypeToken.of(ItemStack.class)));
+        Optional<ItemStack> headwear = Optional.ofNullable(
+                value.getNode("headwear").getValue(TypeToken.of(ItemStack.class)));
+        Optional<ItemStack> chestplate = Optional.ofNullable(
+                value.getNode("chestplate").getValue(TypeToken.of(ItemStack.class)));
+        Optional<ItemStack> leggings = Optional.ofNullable(
+                value.getNode("leggings").getValue(TypeToken.of(ItemStack.class)));
+        Optional<ItemStack> boots = Optional.ofNullable(
+                value.getNode("boots").getValue(TypeToken.of(ItemStack.class)));
+        Optional<ItemStack> offHand = Optional.ofNullable(
+                value.getNode("offHand").getValue(TypeToken.of(ItemStack.class)));
         Map<SlotIndex, ItemStack> main = new HashMap<>();
         for (ConfigurationNode indexNode : value.getNode("main").getChildrenMap().values()) {
             SlotIndex index = SlotIndex.of(indexNode.getKey());
@@ -40,11 +45,11 @@ public class KitSerializer implements TypeSerializer<Kit> {
     @Override
     public void serialize(TypeToken<?> type, Kit obj, ConfigurationNode value) throws ObjectMappingException {
         value.getNode("id").setValue(obj.getId());
-        value.getNode("headwear").setValue(TypeToken.of(ItemStack.class), obj.getHeadwear().get());
-        value.getNode("chestplate").setValue(TypeToken.of(ItemStack.class), obj.getChestplate().get());
-        value.getNode("leggings").setValue(TypeToken.of(ItemStack.class), obj.getLeggings().get());
-        value.getNode("boots").setValue(TypeToken.of(ItemStack.class), obj.getBoots().get());
-        value.getNode("offHand").setValue(TypeToken.of(ItemStack.class), obj.getOffHand().get());
+        value.getNode("headwear").setValue(TypeToken.of(ItemStack.class), obj.getHeadwear().orElse(null));
+        value.getNode("chestplate").setValue(TypeToken.of(ItemStack.class), obj.getChestplate().orElse(null));
+        value.getNode("leggings").setValue(TypeToken.of(ItemStack.class), obj.getLeggings().orElse(null));
+        value.getNode("boots").setValue(TypeToken.of(ItemStack.class), obj.getBoots().orElse(null));
+        value.getNode("offHand").setValue(TypeToken.of(ItemStack.class), obj.getOffHand().orElse(null));
         for (SlotIndex index : obj.getMain().keySet()) {
             ConfigurationNode itemNode = value.getNode("main").getNode(index.getValue().toString());
             itemNode.setValue(TypeToken.of(ItemStack.class), obj.getMain().get(index));
